084f4ee2637f698f2a936f23b4d3da73fcc51f81
Mttw9076 <mtsz.plt@gmail.com>
2025-11-20 15:48:40 +0100

    Initial commit with project files

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+        Console.Write("Enter first number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("Enter second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double result = op switch
+        {
+            "+" => a + b,
+            "-" => a - b,
+            "*" => a * b,
+            "/" => b != 0 ? a / b : double.NaN,
+            _ => double.NaN
+        };
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
