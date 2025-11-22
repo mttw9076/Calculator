1943e42e8f6d7b8a7914d5bee9956f0a6a13c883
Mttw9076 <mtsz.plt@gmail.com>
2025-11-22 10:59:30 +0100

    Second commit

--- Program.cs
+++ Program.cs
@@ -4,15 +4,40 @@ class Program
 {
     static void Main()
     {
+        while(true)
+        {
+        Console.WriteLine("Type 'exit' to quit or press Enter to continue.");
+        string? input = Console.ReadLine();
+        if (input == null)
+            {
+                 Console.WriteLine("Brak danych wejściowych!");
+                    return;
+        }
+        if(input.ToLower() == "exit")
+            break;
+        
         Console.WriteLine("Simple Calculator");
         Console.Write("Enter first number: ");
-        double a = Convert.ToDouble(Console.ReadLine());
+         if (!double.TryParse(Console.ReadLine(), out double a))
+            {
+                Console.WriteLine("Nieprawidłowa liczba!");
+                continue;
+            }
 
         Console.Write("Enter operator (+, -, *, /): ");
-        string op = Console.ReadLine();
+        string? op = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(op))
+            {
+                Console.WriteLine("Brak operatora!");
+                continue;
+            }
 
         Console.Write("Enter second number: ");
-        double b = Convert.ToDouble(Console.ReadLine());
+          if (!double.TryParse(Console.ReadLine(), out double b))
+            {
+                Console.WriteLine("Nieprawidłowa liczba!");
+                continue;
+            }
 
         double result = op switch
         {
@@ -22,7 +47,17 @@ class Program
             "/" => b != 0 ? a / b : double.NaN,
             _ => double.NaN
         };
-
+        if(double.IsNaN(result))
+        {
+            Console.WriteLine("Nie można dzielić przez zero lub nieprawidłowy operator.");
+        }
+        else    
         Console.WriteLine($"Result: {result}");
+
+        }
+
+
+        
+
     }
 }
